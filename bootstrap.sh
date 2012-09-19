1cf570aab13e6d8d7f504f89ac9ef2d435dd65d9
root <root@ip-10-170-225-134.us-west-1.compute.internal>
2012-09-19 05:42:43 +0000

    compiling for older boost

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,6 @@
 
 # To be safe include -I flag
 autoreconf --force --verbose --install
-./configure --config-cache $*
+./configure --config-cache --with-thriftpath=/opt/thrift --with-fb303path=/opt/fb303 --prefix=/opt/scribe $*
+make 
+make install
--- src/file.cpp
+++ src/file.cpp
@@ -245,7 +245,11 @@ void StdFile::listImpl(const std::string& path, std::vector<std::string>& _retur
       boost::filesystem::directory_iterator dir_iter(path), end_iter;
 
       for ( ; dir_iter != end_iter; ++dir_iter) {
+#if BOOST_FILESYSTEM_VERSION >= 3
           _return.push_back(dir_iter->path().filename().string());
+#else
+	_return.push_back(dir_iter->filename());
+#endif
       }
     }
   } catch (const std::exception& e) {
